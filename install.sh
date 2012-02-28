abcf0976b5aa79034e38567d1d0bf786f794fa7b
jpsilvashy <jpsilvashy@gmail.com>
2012-02-28 13:35:54 -0800

    move to regular bin path location and just copy files there

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 
 # Set up the environment. Respect $VERSION if it's set.
   set -e
-  IPA_ROOT="/usr/local/bin/ipa"
+  IPA_ROOT="/usr/local/bin"
   IPA_BIN="$IPA_ROOT/ipa"
   [[ -z "$VERSION" ]] && VERSION=0.0.1
 
@@ -14,16 +14,11 @@
 
 # Remove old versions and create new directories
   rm -fr "$IPA_BIN"
-  mkdir -p "$IPA_ROOT"
   cd "$IPA_ROOT"
 
 # Download latest script
   curl -s https://raw.github.com/mattswe/ipa/master/ipa > ipa
   chmod a+x ipa
-
-# Link to location in users PATH
-  echo "*** Linking..."
-  ln -sfn "$IPA_BIN" /usr/local/bin
 
 # All done!
   echo "*** Installed"
